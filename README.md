e263cf89ac8ca8809fc09031a505ad9cbdfe2ad2
hpstream <915414300@qq.com>
2020-02-27 12:47:27 +0800

    feat:基本环境搭建完毕

---
+++ README.md
@@ -0,0 +1,11 @@
+# 打包工具使用 
+> rollup
+# web调试工具使用 
+> parcel
+
+需要完成模块
+  - [ ] 响应式数据 Object.defineProperty (observer)
+  - [ ] 响应式数据 proxy (reactivity)
+  - [ ] 发布订阅模式
+  - [ ] htmltoAst 也就是json
+  - [ ] vDom 与 dom diff
---
+++ src/observer/index.js
@@ -0,0 +1,10 @@
+
+console.log(3);
+function h(params) {
+  return params;
+}
+var jsx = (<p>hellow world</p>)
+
+console.log('------------------------------------');
+console.log(jsx);
+console.log('------------------------------------');
