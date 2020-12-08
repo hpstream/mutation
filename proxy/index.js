77db63c79504fd02e8c5a9c3c0db9d20f4adf474
黄鹏 <915414300@qq.com>
2020-12-08 14:52:01 +0800

    feat: 完成debug 调试

--- proxy/index.js
+++ proxy/index.js
@@ -1,15 +1,27 @@
 
-// const {produce} = require('immer')
-const {produce} = require('./immer')
+const {produce} = require('immer')
+// const {produce} = require('./immer')
 let baseState = {
-  home:{name:'zhufeng',arr:[1]},
+  home:{name:'爸爸',arr:[1]},
   b:{}
 }
-debugger
-let nextState = produce(baseState, (draft) => {
-    draft.home.name = '珠峰';
-    draft.home.arr.push(2);
+
+// let nextState = produce(baseState, (draft) => {
+//     draft.home.name = '妈妈';
+// })
+// console.log(baseState.home === nextState.home);
+// console.log(baseState.home.arr === nextState.home.arr)
+// console.log(baseState.b === nextState.b);
+
+let p = new Proxy(baseState,{
+  get(tartget, key) {
+      console.log('get', tartget, key)
+      return tartget[key]
+  },
+  set(tartget,key,value){
+    console.log('set', tartget, key, value)
+    tartget[key]=value
+  }
 })
-console.log(baseState.home , nextState.home);
-console.log(baseState.home.arr === nextState.home.arr)
-console.log(baseState.b === nextState.b);
+// p.home = 'mama1'
+p.home.name = 'mama1'
